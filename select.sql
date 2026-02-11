1ab99f80f6bba1f3b92cec302027a5107d4ae77f
NeuralK <kevinmorenorus@gmail.com>
2026-02-11 14:14:40 -0300

    Create select.sql

---
+++ select.sql
@@ -0,0 +1,8 @@
+INSERT INTO `untitled_table_4` (`name`, `age`, `email`, `capibara`) VALUES
+('zapato', 38, 'micojones@gmail.com', 1);
+
+SELECT * FROM untitled_table_4
+
+SELECT name FROM untitled_table_4;
+
+SELECT name, age FROM untitled_table_4;
