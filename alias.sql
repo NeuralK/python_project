cb1f6d0e0761e021e46fb1d0acf2dedad77f21f1
NeuralK <kevinmorenorus@gmail.com>
2026-02-27 14:57:54 -0300

    Create alias.sql

---
+++ alias.sql
@@ -0,0 +1,4 @@
+SELECT name, email as 'cojones' FROM untitled_table_4 WHERE name = 'zapato';
+
+SELECT concat(name, ' edad: ', age) FROM untitled_table_4;
+SELECT concat("nombre: ", name, ', edad: ', age) as "Nombre y edad:" FROM untitled_table_4;
