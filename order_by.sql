089329746848e0fc9ce2a46ec173299d9681702b
NeuralK <kevinmorenorus@gmail.com>
2026-03-10 17:20:36 -0300

    Create order_by.sql

---
+++ order_by.sql
@@ -0,0 +1,4 @@
+SELECT * FROM untitled_table_4 order by age
+SELECT * FROM untitled_table_4 order by age DESC
+SELECT name FROM untitled_table_4 WHERE capibara=0 order by age
+SELECT * FROM untitled_table_4 WHERE capibara = 0 ORDER by age
