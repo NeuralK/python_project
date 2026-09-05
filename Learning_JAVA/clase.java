95345bbaa5cf99ebe5b5db806f4d06913a62f6ff
NeuralK <kevinmorenorus@gmail.com>
2026-09-05 11:14:58 -0300

    Learning_in_JAVA

---
+++ Learning_JAVA/clase.java
@@ -0,0 +1,25 @@
+package juego;
+
+class clase {
+
+    public static void main(String[] args) {
+        
+
+        String [] juego = {"MINECRAFT","CS2","PUBG","WARFRAME", "VALORANT","FIFA","GTA V","FORTNITE","LEAGUE OF LEGENDS","ALBION ONLINE"};
+    
+        System.out.println("En la posicion 3 esta el juego: " + juego[3]);
+        System.out.println("El numero total de juegos es: " + juego.length);
+        System.out.println("El juego en la posicion 10 es: " + juego[9]);
+
+        for (int i = 0; i <= 3; i++) {
+           System.out.println(juego[i]);
+
+    }
+    
+    
+    }
+
+
+
+    
+}
